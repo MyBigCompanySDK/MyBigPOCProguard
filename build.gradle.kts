2dfc33ba6d3047026d7d3bdd98c1b64eafe89523
Thomas Huyghues <thomas.huyghues@miam.tech>
2023-09-15 18:02:21 +0200

    gradle publish config

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,17 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.android.library") version "8.1.0" apply false
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0" apply false
 }
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("io.github.gradle-nexus:publish-plugin:1.1.0")
+    }
+}
+ext["signing.keyId"] = "D8C241BC"
+ext["signing.password"] = "D@ftpunK76"
+ext["signing.secretKeyRingFile"] = "SecretRingKey.gpg"
--- mybigsdk-core/build.gradle.kts
+++ mybigsdk-core/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.mybigcompanysdk.mybigsdk-core"
+    namespace = "com.mybigcompanysdk.mybigsdk_core"
     compileSdk = 34
 
     defaultConfig {
@@ -17,7 +17,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -34,7 +33,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
@@ -42,3 +40,39 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+ext {
+    var PUBLISH_GROUP_ID = "io.github.MyBigCompanySDK"
+    var PUBLISH_VERSION = "1.0.0"
+    var PUBLISH_ARTIFACT_ID = "MyBigPOCProguard"
+    var PUBLISH_DESCRIPTION = "my big sdk poc"
+    var PUBLISH_URL = "https://github.com/MyBigCompanySDK/MyBigPOCProguard"
+    var PUBLISH_LICENSE_NAME = "Apache License 2.0"
+    var PUBLISH_LICENSE_URL =
+        "https://github.com/MyBigCompanySDK/MyBigPOCProguard/blob/master/LICENSE"
+    var PUBLISH_DEVELOPER_ID = "TheScreamer74"
+    var PUBLISH_DEVELOPER_NAME = "Thomas Huyghues"
+    var PUBLISH_DEVELOPER_EMAIL = "thomas.huyghues@miam.tech"
+    var PUBLISH_SCM_CONNECTION =
+        "scm:git:github.com/MyBigCompanySDK/MyBigPOCProguard.git"
+    var PUBLISH_SCM_DEVELOPER_CONNECTION =
+        "scm:git:ssh://github.com/MyBigCompanySDK/MyBigPOCProguard.git"
+    var PUBLISH_SCM_URL =
+        "https://github.com/MyBigCompanySDK/MyBigPOCProguard/tree/master"
+}
+
+val PUBLISH_GROUP_ID by extra ( "io.github.MyBigCompanySDK")
+val PUBLISH_VERSION by extra ("1.0.0")
+val PUBLISH_ARTIFACT_ID by extra("MyBigPOCProguard")
+val PUBLISH_DESCRIPTION by extra ("my big sdk poc")
+val PUBLISH_URL by extra ("https://github.com/MyBigCompanySDK/MyBigPOCProguard")
+val PUBLISH_LICENSE_NAME by extra ("Apache License 2.0")
+val PUBLISH_LICENSE_URL by extra ("https://github.com/MyBigCompanySDK/MyBigPOCProguard/blob/master/LICENSE")
+val PUBLISH_DEVELOPER_ID by extra ("TheScreamer74")
+val PUBLISH_DEVELOPER_NAME by extra ("Thomas Huyghues")
+val PUBLISH_DEVELOPER_EMAIL by extra ("thomas.huyghues@miam.tech")
+val PUBLISH_SCM_CONNECTION by extra ("scm:git:github.com/MyBigCompanySDK/MyBigPOCProguard.git")
+val PUBLISH_SCM_DEVELOPER_CONNECTION by extra ("scm:git:ssh://github.com/MyBigCompanySDK/MyBigPOCProguard.git")
+val PUBLISH_SCM_URL by extra ("https://github.com/MyBigCompanySDK/MyBigPOCProguard/tree/master")
+
+apply(from = "${rootProject.projectDir}/scripts/publish-module.gradle")
